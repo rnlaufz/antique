d54c6269486430d480b2ce173098e7eeb264b91f
Regina Zaripova <rnloftz@gmail.com>
2021-03-27 20:25:10 +1000

    fininshed about page

--- about.php
+++ about.php
@@ -10,20 +10,41 @@
 </head>
 <body>
     <div id="about-content">
-        <div id="nav-bar">
-        <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="events.php">Events</a></li>
-        <li><a href="location.php">Location</a></li>
-        <li><a href="contact.php">Contact</a></li>
-        </ul>
+        <div id="info-content">
+        <h2>About Us</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ex pariatur vero consequuntur, vitae ipsum suscipit blanditiis exercitationem, iure iste quibusdam explicabo laborum nulla? Id libero ex qui est vero ratione officiis et eum nobis veniam ea voluptates, culpa, minus aspernatur cupiditate maxime alias velit voluptate. Magnam illo qui ipsum?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla aliquid blanditiis quas, minus at quasi minima voluptas ullam animi? Dolor, vitae? Quae, provident accusamus ipsum blanditiis eum eaque quasi enim?</p>
+        <a href="events.php" class="btn">Events</a>
         </div>
-        <div id="info-content"></div>
         <div id="img-content">
         <div id="circle-pieces">
                 <div class="piece" id="piece-one"></div>
                 <div class="piece" id="piece-two"></div>
         </div>
+        </div>
+          <!-- Navigation -->
+          <div id="nav-dots-container">
+            <div class="nav-dots">
+                <a href="index.php">
+                <div class="dot"></div>
+               <div class="hide">Home</div> 
+                </a>
+                <a href="about.php">
+                <div class="dot active"></div>
+               <div class="hide">About</div> 
+                </a>
+                <a href="events.php">
+                <div class="dot"></div>
+               <div class="hide">Events</div> 
+                </a>
+                <a href="location.php">
+                <div class="dot"></div>
+               <div class="hide">Location</div> 
+                </a>
+                <a href="contact.php">
+                <div class="dot"></div>
+               <div class="hide">Contact</div> 
+                </a>
+            </div>
         </div>
     </div>
 </body>
