4773ee1d594c494d9c14c36edbcd5aa437c001a5
Regina Zaripova <rnloftz@gmail.com>
2021-04-11 18:47:29 +1000

    added admin login page with ux styling and markup

---
+++ loginadm.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/style.css">
+    <link rel="shortcut icon" href="stylesheets/img/favicon.png" type="image/x-icon">
+    <title>Antique | Login</title>
+</head>
+<body>
+    <div class="login-container">
+    <div id="login-card">
+    <h3>Antique Admin</h3>
+    <form action="">
+    <div class="form-control">
+    <input type="text" placeholder="Login" />
+    </div>
+    <div class="form-control">
+    <input type="password" placeholder="Password" />
+    </div>
+    <div class="form-control">
+    <input type="submit" value="Submit"  />
+  
+    </div>
+    <br>
+    </form>
+    </div>
+    <div id="footer">
+        <p>Admin page</p>
+    
+   <p>Antique Copyright &copy; 2021 All rights reserved | Build by /RZ</p>
+   </div>
+    </div>
+</body>
+</html>
