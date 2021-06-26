1a179cda1f5eb7a93bfe37437d48b86c9e8eb9a4
Regina Zaripova <rnloftz@gmail.com>
2021-06-26 17:55:13 +1000

    moved adaptive medias to own file | changed main style css for better adaptivness

--- contact.php
+++ contact.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylesheets/style.css">
+    <link rel="stylesheet" href="stylesheets/adaptive.css">
     <link rel="shortcut icon" href="stylesheets/img/favicon.png" type="image/x-icon">
     <title>Antique | Contact Us</title>
 </head>
--- events.php
+++ events.php
@@ -12,6 +12,7 @@ require 'includes/handlers/get_event.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylesheets/style.css">
+    <link rel="stylesheet" href="stylesheets/adaptive.css">
     <link rel="shortcut icon" href="stylesheets/img/favicon.png" type="image/x-icon">
     <title>Antique | Events</title>
 </head>
